e16e57c119d0e1be5f3535e14fa56d846174f7eb
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-12-16 13:49:48 +0200

    Show cta-section in Home only for Guests

--- client/explore-Bulgaria/src/app/home/home.component.ts
+++ client/explore-Bulgaria/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+constructor(
+    public userService: UserService,
+  ) {}
+
+  get isLoggedIn(): boolean {
+    return this.userService.isLoggedIn();
+  }
 }
