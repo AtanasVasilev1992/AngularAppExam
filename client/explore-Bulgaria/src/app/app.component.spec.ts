b73f69f8bc2a8b447f71ae53960b0b9c75dddafc
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-12-17 21:13:31 +0200

    Fix err in app.componnent.spec.ts

--- client/explore-Bulgaria/src/app/app.component.spec.ts
+++ client/explore-Bulgaria/src/app/app.component.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AuthenticateComponent } from './authenticate/authenticate.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        AuthenticateComponent 
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
