f96f3e9ca77a15f309222d0db2cd6bb29ef3b184
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-12-19 00:18:38 +0200

    Add Angular animations to 404
    Changes in other css files

--- client/explore-Bulgaria/src/app/not-found/not-found.component.ts
+++ client/explore-Bulgaria/src/app/not-found/not-found.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
+  animations: [
+    trigger('containerAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'scale(0.3)' }),
+        animate('0.5s ease-out', 
+          style({ opacity: 1, transform: 'scale(1)' })
+        )
+      ])
+    ])
+  ]
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent {}
