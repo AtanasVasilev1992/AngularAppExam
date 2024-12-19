2c13b8528e10e812b797a04dac1c71beb86bec51
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-12-19 21:22:18 +0200

    Remove isLoggedIn from about

--- client/explore-Bulgaria/src/app/about/about.component.ts
+++ client/explore-Bulgaria/src/app/about/about.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { UserService } from '../user/user.service';
 
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {
-    constructor(public userService: UserService) {}
-
-    get isLoggedIn(): boolean {
-        return this.userService.isLoggedIn();
-    }
-}
+export class AboutComponent {}
