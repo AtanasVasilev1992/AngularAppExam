f382525da85915e9fca3895bee9e2ca81e6a12db
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-12-19 02:09:27 +0200

    Update Footer and Loader

--- client/explore-Bulgaria/src/app/core/footer/footer.component.ts
+++ client/explore-Bulgaria/src/app/core/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+    constructor(public userService: UserService) {}
+  
+    get isLoggedIn(): boolean {
+      return this.userService.isLoggedIn();
+    }
 }
